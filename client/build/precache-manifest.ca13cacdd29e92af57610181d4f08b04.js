94a42b69358972e7313f589390527bb2ceaff5f6
Omar Finol-Evans <omarfevans@gmail.com>
2020-04-04 19:34:09 -0700

    Forgot to include actual build folder

---
+++ client/build/precache-manifest.ca13cacdd29e92af57610181d4f08b04.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7996fa8c0f59975199de516834df6630",
+    "url": "/index.html"
+  },
+  {
+    "revision": "936e54215a620c1403d4",
+    "url": "/static/css/2.c837e8ca.chunk.css"
+  },
+  {
+    "revision": "4185b02babe370be651b",
+    "url": "/static/css/main.b9f1dffe.chunk.css"
+  },
+  {
+    "revision": "936e54215a620c1403d4",
+    "url": "/static/js/2.c2ea28fe.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c2ea28fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4185b02babe370be651b",
+    "url": "/static/js/main.61761b5e.chunk.js"
+  },
+  {
+    "revision": "cf7f6f80381cb3a854da",
+    "url": "/static/js/runtime-main.c2932262.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
